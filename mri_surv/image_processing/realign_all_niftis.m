5ebb2445b02c518866ac7b4bc9cd4ce596605dbf
Meagan Lauber <90205073+mlauber1999@users.noreply.github.com>
2022-07-14 12:55:51 -0400

    Update realign_all_niftis.m
    Added clarifying notes

--- mri_surv/image_processing/realign_all_niftis.m
+++ mri_surv/image_processing/realign_all_niftis.m
@@ -1,3 +1,4 @@
+#script to move niftis to a new folder once they are realigned
 function rids = realign_all_niftis(outputfolder, suffix)
 %% main
 if nargin < 1
@@ -7,8 +8,8 @@ if nargin < 2
     suffix = '';
 end
 
-MRI_folder_init = ['/data2/MRI_PET_DATA/raw_data/MRI_nii' suffix filesep];
-fnames = dir(MRI_folder_init);
+MRI_folder_init = ['/data2/MRI_PET_DATA/raw_data/MRI_nii' suffix filesep]; #goes through this folder 
+fnames = dir(MRI_folder_init); 
 fnames = {fnames.name};
 rids = arrayfun(@(x) regexp(x,'^([0-9]{4}).*\.nii$','tokens'), fnames, 'uniformoutput', false);
 rids = [rids{:}];
@@ -18,6 +19,7 @@ rids_mri = [rids{:}];
 rids = rids_mri;
 
 MRI_folder_new = [outputfolder '/ADNI_MRI_nii_recenter' suffix];
+#moves the niftis to this new folder after realigning them 
 
 if ~exist(outputfolder,'dir')
     mkdir(outputfolder)
