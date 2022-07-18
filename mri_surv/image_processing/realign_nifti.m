8beb3a39225db354465facb3afef6f9f9c662401
Meagan Lauber <90205073+mlauber1999@users.noreply.github.com>
2022-07-18 13:07:13 -0400

    Update realign_nifti.m
    corrected comments from # to %

--- mri_surv/image_processing/realign_nifti.m
+++ mri_surv/image_processing/realign_nifti.m
@@ -13,12 +13,12 @@ for i=1:length(listing)
         fname = join([listing(i).folder filesep listing(i).name],'');
         system(['rsync -a ' fname ' ' folder_name '/../tmp/']);
         if ismember(rid{1},rid_list)
-            Vo = SetOriginToCenter([folder_name '/../tmp/' listing(i).name]); #set origin to center realigns them 
+            Vo = SetOriginToCenter([folder_name '/../tmp/' listing(i).name]); %set origin to center realigns them 
             [data] = spm_read_vols(Vo);
             Vo.fname = [folder_name filesep rid{1}{1} '_' modality '.nii'];
-            fnames = [fnames, Vo.fname]; #file you get from the core 
+            fnames = [fnames, Vo.fname]; %file you get from the core 
             if ~exist(Vo.fname, 'file')
-                spm_write_vol(Vo, data); #writes the new volume at a new location 
+                spm_write_vol(Vo, data); %writes the new volume at a new location 
             end
         else
             disp(['file ' listing(i).name ' not in list of RIDs'])
